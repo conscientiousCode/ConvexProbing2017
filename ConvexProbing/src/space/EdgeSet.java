18a1c018c38ffd7248216bb4ec3f11e515091190
conscientiousCode <conscientiouscode@outlook.com>
2017-05-14 15:09:51 -0700

    Work

--- ConvexProbing/src/space/EdgeSet.java
+++ ConvexProbing/src/space/EdgeSet.java
@@ -1,17 +1,6 @@
 package space;
-<<<<<<< HEAD
-=======
-
-<<<<<<< HEAD
-public class EdgeSet {public EdgeSet() {
-}}
-=======
->>>>>>> branch 'master' of https://github.com/conscientiousCode/ConvexProbing2017.git
 
 public class EdgeSet{
 	
 	
 }
-
-
->>>>>>> branch 'master' of https://github.com/conscientiousCode/ConvexProbing2017.git
