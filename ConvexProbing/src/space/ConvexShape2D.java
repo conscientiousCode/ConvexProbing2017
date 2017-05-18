70f03077dbd839282e6923fdb4fd15ac1d015df9
conscientiousCode <conscientiouscode@outlook.com>
2017-05-18 14:32:26 -0700

    Added a required constructor to EdgeSet

--- ConvexProbing/src/space/ConvexShape2D.java
+++ ConvexProbing/src/space/ConvexShape2D.java
@@ -1,8 +1,18 @@
 package space;
 
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Random;
+import java.util.Set;
+
 
 public class ConvexShape2D implements ConvexShape{
+	
+	private static final Random rdm = new Random();
+	
+	
+	private Set<Point> vertices = new HashSet<Point>();
+	
 
 	@Override
 	public Iterator<Point> iterator() {
--- ConvexProbing/src/space/EdgeSet.java
+++ ConvexProbing/src/space/EdgeSet.java
@@ -2,5 +2,7 @@ package space;
 
 public class EdgeSet{
 	
-	
+	public EdgeSet(Point[][] pointPairs){
+		/*TO DO: Initialize an edgeset from a 2d array, where the array stored in each primary index is guaranteed to have exactly 2 elements*/
+	}
 }
