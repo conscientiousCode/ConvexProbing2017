ba7f64c9935a43fbe6ce4e61dfa4a5283701b521
conscientiousCode <conscientiouscode@outlook.com>
2017-06-11 21:50:45 -0700

    Added some tests for ConvexShape2D

--- ConvexProbing/src/space/ConvexShape2D.java
+++ ConvexProbing/src/space/ConvexShape2D.java
@@ -18,25 +18,20 @@ public class ConvexShape2D implements ConvexShape{
 	
 	private Set<Point> vertices;
 	private EdgeSet edges;
+	private RandomEllipse ellipse;
 	
 	
 	public ConvexShape2D(int numOfVertices){
-		/*TO DO: IMPLEMENT CHECK TO ensure numOFVertices can be constructed given MINIMUM_POINT_DISTANCE*/
+		/*TO DO: IMPLEMENT CHECK TO ensure numOfVertices can be constructed given MINIMUM_POINT_DISTANCE*/
 		//Get Points on an ellipse and ensure that they are a sufficient distance away.
+		
+		ellipse = new RandomEllipse(); // Create a random ellipse to sample points from
 		
 		selectPoints(numOfVertices); // initializes vertices
 		makeEdgeSet(numOfVertices);  // initializes
 	}
 	
 	protected void selectPoints(int numOfVertices){
-		// construct ellipse generation
-				double xBound = 1 + rdm.nextDouble()*(MAX_BOUND-1);//Ensure that x width is at least the size of 1
-				double yBound = 1 + rdm.nextDouble()*(MAX_BOUND-1);
-				java.util.function.Supplier<double[]> ellipsePoint = () -> { // This is a lambda function
-					double radians = Math.PI*2*rdm.nextDouble();
-					return new double[]{xBound*Math.cos(radians), yBound*Math.sin(radians)};
-				};
-				
 		// construct points and check if they are far enough apart
 				java.util.function.BiFunction<double[], double[], Double> distanceBetweenPoints = (double[] p1, double[] p2) -> {
 					return (Math.sqrt(Math.pow(p1[0]-p2[0],2)+ Math.pow(p1[1]-p2[1], 2)));
@@ -48,7 +43,7 @@ public class ConvexShape2D implements ConvexShape{
 				boolean pointFailed = false;
 				/*Possibly Non-terminating by chance...*/
 				while(numOfConstructedVertices != numOfVertices){
-					newPoint = ellipsePoint.get();
+					newPoint = ellipse.getRandomPoint();
 					for(int i = 0; i < numOfConstructedVertices; i++){
 						if(distanceBetweenPoints.apply(pointSet[i], newPoint) <= MINIMUM_POINT_DISTANCE){
 							pointFailed = true;
@@ -77,27 +72,14 @@ public class ConvexShape2D implements ConvexShape{
 			quadrantsList[quadrant(point)-1].add(point);
 		}
 		
-		/*Compares points where the point associated with pi = 0 is the lowest on the ellipse,
-		 * and 2*pi-epsilon is the highest 
-		 * */
-		java.util.Comparator<Point> counterClockwiseOrientation = (Point p1, Point p2) -> {
-			/*int comparison = quadrant(p1) - quadrant(p2);
-			if(comparison != 0){
-				return comparison;
-			}else*/ if(p1.equals(p2)){
-				return 0;
-				
-			}else if(quadrant(p1) == 1 || quadrant(p1) == 2){
-				return(p1.getAxisValue(0).doubleValue() > p2.getAxisValue(0).doubleValue())?(-1):(1);
-			}else{ // quadrant 3 or 4
-				return(p1.getAxisValue(0).doubleValue() > p2.getAxisValue(0).doubleValue())?(1):(-1);
-			}
+		java.util.Comparator<Point> pointOrderer = (Point p1, Point p2) -> {
+			 return pointOrder(p1,p2);
 		};
 		
 		Point[][] quadrants = new Point[4][];
 		for(int i = 0; i < 4; i++){
 			quadrants[i] = quadrantsList[i].toArray(new Point[0]);
-			Arrays.sort(quadrants[i], counterClockwiseOrientation);
+			Arrays.sort(quadrants[i], pointOrderer);
 		}
 		
 		Point[] orderedPoints = new Point[numOfVertices];
@@ -117,8 +99,18 @@ public class ConvexShape2D implements ConvexShape{
 		
 	}
 	
+	/*
+	 * note that the edge case (0,0) never occurs because the radius of the random ellipse is > 0.
+	 * returns the quadrant that Point point exists in, if the point falls on an axis line, it goes to the quadrant that is just previous to the line
+	 * */
 	protected static int quadrant(Point point){
-		if(point.getAxisValue(0).doubleValue() > 0){
+		if(point.getAxisValue(0).doubleValue() == 0){ // edge case of (0,y)
+			return (point.getAxisValue(1).doubleValue() > 0)?(1):(3); // If y values is greater than zero, 1, otherwise 3
+		}else if(point.getAxisValue(1).doubleValue() == 0){//edge case of (0,x)
+			/* For consistency, (x,0) is considered to be in quadrant 4 as it is the previous quadrant assuming we arrived at
+			 * this point after some number of counter-clockwise revolutions*/
+			return (point.getAxisValue(0).doubleValue() > 0)?(4):(2);
+		}else if(point.getAxisValue(0).doubleValue() > 0){//none-edge case
 			if(point.getAxisValue(1).doubleValue() > 0){
 				return 1;
 			}else{
@@ -132,7 +124,6 @@ public class ConvexShape2D implements ConvexShape{
 			}
 		}
 	}
-
 
 	@Override
 	public Iterator<Point> iterator() {
@@ -158,6 +149,43 @@ public class ConvexShape2D implements ConvexShape{
 	@Override
 	public boolean hasEdgeBetween(Point p1, Point p2) {
 		return edges.containsEdge(p1, p2);
+	}
+	
+	private class RandomEllipse{
+		private double xBound;
+		private double yBound;
+		
+		public RandomEllipse(){
+			//Set Max width and height
+			xBound = 1 + rdm.nextDouble()*(MAX_BOUND-1);//Ensure that x width is at least the size of 1
+			yBound = 1 + rdm.nextDouble()*(MAX_BOUND-1);
+		}
+		
+		public double[] getRandomPoint(){
+			double radians = Math.PI*2*rdm.nextDouble();
+			return new double[]{xBound*Math.cos(radians), yBound*Math.sin(radians)};
+		}
+		
+	}
+	
+	/*
+	 * One point is greater than another if its value pair occurs later given f: t ->R^2 : f(t) = [cos[t], sin[t], t = [0, 2PI]
+	 * */
+	protected static int pointOrder(Point p1, Point p2) {
+		if (quadrant(p1) == quadrant(p2)) {
+			if (p1.equals(p2)) {
+				return 0;
+			} else {
+				if (quadrant(p1) == 1 || quadrant(p1) == 2) {
+					return (p1.getAxisValue(0).doubleValue() > p2.getAxisValue(0).doubleValue()) ? (-1) : (1);
+				} else {
+					return (p1.getAxisValue(0).doubleValue() < p2.getAxisValue(0).doubleValue()) ? (-1) : (1);
+				}
+			}
+		} else {
+			return quadrant(p1) - quadrant(p2);
+		}
+
 	}
 
 }
